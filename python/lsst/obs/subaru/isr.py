fbef799905090c51a3ea08be701b45b01c7067c9
Lauren MacArthur <lauren@astro.princeton.edu>
2016-03-01 14:12:31 -0500

    Some ugly transformations to try to mimic HSC behavior

--- python/lsst/obs/subaru/isr.py
+++ python/lsst/obs/subaru/isr.py
@@ -379,8 +379,15 @@ class SubaruIsrTask(IsrTask):
         if self.config.doSaturation:
             if not doRotateCalib:
                 self.saturationInterpolation(ccdExposure)
-            else:
-                with self.rotated(ccdExposure) as exp:
+            elif nQuarter == 2:
+                with self.rotated(ccdExposure, 2) as exp: # using rot 180 to match HSC
+                    self.saturationInterpolation(exp)
+            elif nQuarter == 1:
+                with self.rotated(ccdExposure, 2) as exp: # using rot 180 to match HSC
+                    with self.flipped(exp) as exp: # bc HSC transposes 90/270 ccds
+                        self.saturationInterpolation(exp)
+            elif nQuarter == 3:
+                with self.flipped(ccdExposure) as exp: # bc HSC transposes 90/270 ccds
                     self.saturationInterpolation(exp)
         if doWriteDebug: ccdExposure.writeFits(outDir+"/isr10.fits")
 
@@ -437,6 +444,14 @@ class SubaruIsrTask(IsrTask):
             yield exp
         finally:
             exp.setMaskedImage(afwMath.rotateImageBy90(exp.getMaskedImage(), 4 - nQuarter))
+
+    @contextmanager
+    def flipped(self, exp, flipLR=True, flipTB=False):
+        exp.setMaskedImage(afwMath.flipImage(exp.getMaskedImage(), flipLR, flipTB))
+        try:
+            yield exp
+        finally:
+            exp.setMaskedImage(afwMath.flipImage(exp.getMaskedImage(), flipLR, flipTB))
 
     def applyGains(self, ccdExposure, normalizeGains):
         ccd = ccdExposure.getDetector()
